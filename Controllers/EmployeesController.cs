954351a206803b0e57b47f27b03479891194e2ba
rodrigo.brandao <rodrigo.brandao@gmail.com>
2025-03-05 10:25:31 -0300

    Initial

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,28 @@
+﻿using EmployeeAdminPortal.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeAdminPortal.Controllers
+{
+    //localhost:xxx/api/employees
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public EmployeesController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+
+        }
+
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+            var allEmployees = dbContext.Employees.ToList();
+            return Ok(allEmployees);
+        }
+
+    }
+}
---
+++ Models/Entities/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmployeeAdminPortal.Models.Entities
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; } 
+        public string? Phone { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
+        public required decimal Salary { get; set; }
+    }
+}
