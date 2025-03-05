7b2fac58ccd714f4b09e506def87a0dd6339d64e
rodrigo.brandao <rodrigo.brandao@gmail.com>
2025-03-05 10:41:34 -0300

    Migration scipt for FlyWay

---
+++ Migrations/V1__CreateTable.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [Employees] (
+          [Id] uniqueidentifier NOT NULL,
+          [Name] nvarchar(max) NOT NULL,
+          [Email] nvarchar(max) NOT NULL,
+          [Phone] nvarchar(max) NULL,
+          [Salary] decimal(18,2) NOT NULL,
+          CONSTRAINT [PK_Employees] PRIMARY KEY ([Id])
+      );
